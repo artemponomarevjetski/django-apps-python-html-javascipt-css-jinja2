0cda36d5e3f601e03e697e09ae957ccf732ae60b
Artem Ponomarev <artemponomarevjetski@gmail.com>
2021-02-26 23:02:52 -0800

    First commit

---
+++ django-artemp-portfolio/README.md
@@ -0,0 +1,2 @@
+# artem-p-portfolio
+Shows DS/ML/AI projects done by Artem Ponomarev, Ph.D.
---
+++ django-artemp-portfolio/pages/views.py
@@ -0,0 +1,11 @@
+# pages/views.py
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView): # new
+    template_name = 'about.html'
